36aaae48fb66e7b3eff781cdd2e4f39d81d4ac0e
shawsmith <shawsmith@sina.com>
2023-08-06 15:13:51 +0800

    更新vim配置

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # devconf
-保存了日常开发环境配置文件
-## Emacs
-位于emacs.d目录下
-## Maven
-位于maven目录下
-
+保存了日常开发环境配置文件, 其中包括了：Emacs、Maven、Vim。
--- dwm/config.def.h
+++ dwm/config.def.h
@@ -40,7 +40,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", " ", "", "", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", " ", "", "", "", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
