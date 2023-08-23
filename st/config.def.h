249c4851a1deff3292963ccf25e7e2c040eda83b
shawsmith <shawsmith@sina.com>
2023-08-23 20:57:33 +0800

    change font size

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:size=18:antialias=true:autohint=true";
+static char *font = "Source Code Pro:size=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
