9cdb5f6dda07ce82b0af51c05fbee67857bb5c33
shawsmith <shawsmith@sina.com>
2024-05-30 13:28:44 +0800

    initial commit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,4 @@
-;;; Emacs basic configuration.
+
 (setq inhibit-startup-message t)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
@@ -36,7 +36,7 @@
 (add-hook 'write-file-functions 'delete-trailing-whitespace)
 
 ;;; Font configuration.
-(defvar efs/default-font-family "Hack Nerd Font Mono")
+(defvar efs/default-font-family "Source Code Pro")
 (defvar efs/default-font-size 180)
 (defvar efs/default-variable-font-size 180)
 (set-face-attribute 'default nil
@@ -58,11 +58,10 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 (global-set-key (kbd "C-x b") 'ibuffer)
 
-;;; Melpa package manager & auto renew configuration.
+(setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+                         ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (require 'package)
-(setq package-archives '(("gnu"   . "http://mirrors.cloud.tencent.com/elpa/gnu/")
-                       ("melpa" . "http://mirrors.cloud.tencent.com/elpa/melpa/")))
-
 (package-initialize)
 
 (unless package-archive-contents
@@ -71,17 +70,9 @@
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
-;;; Installed package configuration.
-
 (use-package restart-emacs
   :ensure t)
 
-(use-package zenburn-theme
-  :ensure t
-  :init (load-theme 'zenburn t))
-
-(use-package all-the-icons
-  :ensure t)
 
 (use-package rainbow-delimiters
   :ensure t
@@ -104,11 +95,7 @@
 
 (use-package which-key
   :defer 0
-  :diminish which-key-mode
-  :config
-  (which-key-mode)
-  (setq which-key-idle-delay 0.3))
-
+  :diminish which-key-mode)
 
 ;;; ivy & counsel
 (use-package ivy
@@ -129,10 +116,6 @@
   :config
   (ivy-mode 1))
 
-(use-package ivy-rich
-  :ensure t
-  :init
-  (ivy-rich-mode 1))
 
 (use-package counsel
   :ensure t
@@ -172,20 +155,131 @@
   :ensure t
   :init (exec-path-from-shell-initialize))
 
-(use-package eglot
-  :hook ((c-mode
-          c++-mode
-          java-mode
-          ;; rust-mode
-          ) . eglot-ensure)
-  :bind (("C-c e f" . #'eglot-format)
-         ("C-c e a" . #'eglot-code-actions)
-         ("C-c e i" . #'eglot-code-action-organize-imports)
-         ("C-c e q" . #'eglot-code-action-quickfix))
+(use-package zenburn-theme
+  :ensure t
+  :init (load-theme 'zenburn t))
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
   :config
-  (defun eglot-actions-before-save()
-    (add-hook 'before-save-hook
-              (lambda ()
-                (call-interactively #'eglot-format)
-                (call-interactively #'eglot-code-action-organize-imports))))
-  (add-hook 'eglot--managed-mode-hook #'eglot-actions-before-save))
+  (progn
+    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay        0.5
+          treemacs-directory-name-transformer      #'identity
+          treemacs-display-in-side-window          t
+          treemacs-eldoc-display                   'simple
+          treemacs-file-event-delay                2000
+          treemacs-file-extension-regex            treemacs-last-period-regex-value
+          treemacs-file-follow-delay               0.2
+          treemacs-file-name-transformer           #'identity
+          treemacs-follow-after-init               t
+          treemacs-expand-after-init               t
+          treemacs-find-workspace-method           'find-for-file-or-pick-first
+          treemacs-git-command-pipe                ""
+          treemacs-goto-tag-strategy               'refetch-index
+          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
+          treemacs-hide-dot-git-directory          t
+          treemacs-indentation                     2
+          treemacs-indentation-string              " "
+          treemacs-is-never-other-window           nil
+          treemacs-max-git-entries                 5000
+          treemacs-missing-project-action          'ask
+          treemacs-move-files-by-mouse-dragging    t
+          treemacs-move-forward-on-expand          nil
+          treemacs-no-png-images                   nil
+          treemacs-no-delete-other-windows         t
+          treemacs-project-follow-cleanup          nil
+          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                        'left
+          treemacs-read-string-input               'from-child-frame
+          treemacs-recenter-distance               0.1
+          treemacs-recenter-after-file-follow      nil
+          treemacs-recenter-after-tag-follow       nil
+          treemacs-recenter-after-project-jump     'always
+          treemacs-recenter-after-project-expand   'on-distance
+          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+          treemacs-project-follow-into-home        nil
+          treemacs-show-cursor                     nil
+          treemacs-show-hidden-files               t
+          treemacs-silent-filewatch                nil
+          treemacs-silent-refresh                  nil
+          treemacs-sorting                         'alphabetic-asc
+          treemacs-select-when-already-in-treemacs 'move-back
+          treemacs-space-between-root-nodes        t
+          treemacs-tag-follow-cleanup              t
+          treemacs-tag-follow-delay                1.5
+          treemacs-text-scale                      nil
+          treemacs-user-mode-line-format           nil
+          treemacs-user-header-line-format         nil
+          treemacs-wide-toggle-width               70
+          treemacs-width                           35
+          treemacs-width-increment                 1
+          treemacs-width-is-initially-locked       t
+          treemacs-workspace-switch-cleanup        nil)
+
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+    (when treemacs-python-executable
+      (treemacs-git-commit-diff-mode t))
+
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple)))
+
+    (treemacs-hide-gitignored-files-mode nil))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t d"   . treemacs-select-directory)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-enable-once)
+  :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
+
+(use-package magit
+  :ensure t)
+
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (
+         (c-mode . lsp-deferred)
+         (c++-mode . lsp-deferred)
+         (rust-mode . lsp-deferred)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package rust-mode
+  :ensure t
+  :init
+  (setq rust-format-on-save t))
+(add-hook 'rust-mode-hook #'lsp)
+
+(add-hook 'before-save-hook #'lsp-format-buffer t)
