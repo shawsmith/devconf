6f8b676484f6546289b2fa934aa50fe4c503d077
shawsmith2013 <shawsmith2013@gmail.com>
2025-07-10 14:12:48 +0800

    update joe

---
+++ joe/joe-editor/joe/utag.h
@@ -0,0 +1,15 @@
+/* 
+ *	tags file symbol lookup
+ *	Copyright
+ *		(C) 1992 Joseph H. Allen
+ *
+ *	This file is part of JOE (Joe's Own Editor)
+ */
+#ifndef _JOE_UTAG_H
+#define _JOE_UTAG_H 1
+
+extern int notagsmenu;
+int utagjump(BW *bw);
+int utag(BW *bw);
+
+#endif
